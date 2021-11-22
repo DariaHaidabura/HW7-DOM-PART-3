513c42988d69574fe60ffe9d0fb8ba43e4c345bb
Dasha <dariagaidabura@gmail.com>
2021-11-22 15:58:15 +0200

    changed commit

--- lesson7.js
+++ lesson7.js
@@ -1,36 +1,42 @@
 
 let button = document.getElementById('button');
 let ul = document.getElementById('list');
-var input = document.getElementById('input');
+let input = document.getElementById('input');
+
+button.addEventListener('click', addListItem);
+ul.addEventListener('click', changeList);
+
+function createListItem() {
+  let li = document.createElement("li");
+  li.innerText = input.value;
+  li.className = "li-yellow";
+  let button = document.createElement("button");
+  button.innerText = "Удалить";
+  ul.append(li);
+  li.append(button);
+}
 
 function addListItem() {
-  const li = createListItem(input.value);
-  ul.append(li);
-  input.value = '';
+  if (!input.value) {
+    return input.value;
+  } else {
+    createListItem();
+    input.value = '';
+  }
 }
-
-function createListItem(value) {
-  const li = document.createElement("li");
-  li.innerText = value;
-  const button = createRemoveButton();
-  li.append(button)
-  li.addEventListener("click", changeColor)
-  return li;
-}
-
-function createRemoveButton() {
-  const button = document.createElement("button")
-  button.innerText = "Удалить";
-  button.addEventListener("click", removeItem)
-  return button;
-}
-
-button.addEventListener("click", addListItem);
 
 function changeColor(e) {
-  e.target.classList.toggle("li-green")
+  e.classList.toggle("li-green");
+  e.classList.toggle("li-yellow");
 }
 
-function removeItem(e) {
-  e.target.parentElement.remove()
-}
+function changeList(e) {
+  let target = e.target;
+  if(target.tagName === "BUTTON"){
+    target.closest("li").remove();
+   } else if (target.tagName === "LI") {
+    changeColor(target);
+   } else
+   console.log("error");
+    }
+
